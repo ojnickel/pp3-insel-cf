15bee0ea11b640690db3f05db82b17d1738101cc
Oswaldo Nickel <oswaldo.nickel@gmail.com>
2018-10-17 13:06:50 +0200

    backup overrides and <br /> -> menu title n url

--- mod_menu/pphauptmenue_url.php
+++ mod_menu/pphauptmenue_url.php
@@ -3,51 +3,75 @@
  * @package     Joomla.Site
  * @subpackage  mod_menu
  *
- * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
- *
- * Aenderung OWS Andreas Wagner
- * 20150415
- * Zeilenumbruch ersetzen
+ 
+ Webwerk - Oswaldo Nickel - 17.10.2018
+
+ Änderung in Layout 
+ -> Titel von menu Punkt in 2 Linie teilen -> <br />
+ - Probleme mit dem alten Override -> ampersand (&)
+ |
+ +-> neue Funktion in Joomla Framework -> ampReplace
+
+
  */
 
 defined('_JEXEC') or die;
-jimport( 'joomla.utilities.string' );
 
-// Note. It is important to remove spaces between elements.
-$class = $item->anchor_css ? 'class="' . $item->anchor_css . '" ' : '';
-$title = $item->anchor_title ? 'title="' . $item->anchor_title . '" ' : '';
+$attributes = array();
+
+if ($item->anchor_title)
+{
+	$attributes['title'] = $item->anchor_title;
+}
+
+if ($item->anchor_css)
+{
+	$attributes['class'] = $item->anchor_css;
+}
+
+if ($item->anchor_rel)
+{
+	$attributes['rel'] = $item->anchor_rel;
+}
+
+$linktype = $item->title;
 
 if ($item->menu_image)
 {
-	$item->params->get('menu_text', 1) ?
-	$linktype = '<img src="' . $item->menu_image . '" alt="' . $item->title . '" /><span class="image-title">' . $item->title . '</span> ' :
-	$linktype = '<img src="' . $item->menu_image . '" alt="' . $item->title . '" />';
+	if ($item->menu_image_css)
+	{
+		$image_attributes['class'] = $item->menu_image_css;
+		$linktype = JHtml::_('image', $item->menu_image, $item->title, $image_attributes);
+	}
+	else
+	{
+		$linktype = JHtml::_('image', $item->menu_image, $item->title);
+	}
+
+	if ($item->params->get('menu_text', 1))
+	{
+		$linktype .= '<span class="image-title">' . $item->title . '</span>';
+	}
 }
-else
+
+if ($item->browserNav == 1)
 {
-	$linktype = $item->title;
+	$attributes['target'] = '_blank';
+	$attributes['rel'] = 'noopener noreferrer';
 
+	if ($item->anchor_rel == 'nofollow')
+	{
+		$attributes['rel'] .= ' nofollow';
+	}
+}
+elseif ($item->browserNav == 2)
+{
+	$options = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,' . $params->get('window_open');
+
+	$attributes['onclick'] = "window.open(this.href, 'targetWindow', '" . $options . "'); return false;";
 }
 
-$flink = $item->flink;
-$flink = JFilterOutput::ampReplace(htmlspecialchars($flink));
-
-//OWS Andreas Wagner: Zeilenumbruch ersetzen
 $linktype = JString::str_ireplace("[br]", "<br />", $linktype);
-
-switch ($item->browserNav) :
-	default:
-	case 0:
-?><a <?php echo $class; ?>href="<?php echo $flink; ?>" <?php echo $title; ?>><span><?php echo $linktype; ?></span></a><?php
-		break;
-	case 1:
-		// _blank
-?><a <?php echo $class; ?>href="<?php echo $flink; ?>" target="_blank" <?php echo $title; ?>><span><?php echo $linktype; ?></span></a><?php
-		break;
-	case 2:
-		// Use JavaScript "window.open"
-		$options = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,' . $params->get('window_open');
-			?><a <?php echo $class; ?>href="<?php echo $flink; ?>" onclick="window.open(this.href,'targetWindow','<?php echo $options;?>');return false;" <?php echo $title; ?>><span><?php echo $linktype; ?></span></a><?php
-		break;
-endswitch;
+echo JHtml::_('link', JFilterOutput::ampReplace(htmlspecialchars($item->flink, ENT_COMPAT, 'UTF-8', false)), $linktype, $attributes);
